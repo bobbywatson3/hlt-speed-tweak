e6a6b6e87a7d53c2097291ada6dcf8a808b47c7c
Bobby Watson <robertwatson3@gmail.com>
2015-03-27 16:32:54 -0400

    Initial Commit

---
+++ hlt-speed-tweak.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+file_name = "LapTimer-0131-20150326-093740.hlptrz"
+correction_factor = 0.96
+
+parser = ET.XMLParser(encoding="windows-1252")
+tree = ET.parse(file_name, parser=parser)
+root = tree.getroot()
+
+
+for speed in root.findall('.//obd/speed'):
+    corrected_speed = float(speed.text) * correction_factor
+    speed.text = str(corrected_speed)
+tree.write('corrected_speed.xml')
