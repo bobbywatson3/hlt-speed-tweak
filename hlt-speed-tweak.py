5cde469c910f70d02d0d4272fbd84fb3bc87ab16
Bobby Watson <robertwatson3@gmail.com>
2015-03-28 08:26:29 -0400

    Added support for gzipped LapTimer files

--- hlt-speed-tweak.py
+++ hlt-speed-tweak.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 import xml.etree.ElementTree as ET
-file_name = "LapTimer-0131-20150326-096540.hlptrz"
+import gzip
+
+lap_timer_file = "LapTimer-0131-20150326-093740.hlptrz"
 correction_factor = 0.96
 
-parser = ET.XMLParser(encoding="windows-1252")
-tree = ET.parse(file_name, parser=parser)
-root = tree.getroot()
+if lap_timer_file.endswith('.hlptrz'):
+    print "Unzipping file"
+    lap_timer_file = gzip.open(lap_timer_file, 'rb')
+    lap_timer_string = lap_timer_file.read()
+    root = ET.fromstring(lap_timer_string)
+    for speed in root.findall('.//obd/speed'):
+        corrected_speed = float(speed.text) * correction_factor
+        speed.text = str(corrected_speed)
+    tree = ET.ElementTree(root)
+    corrected_gzip = gzip.open('corrected_obd_speed.hlptrz', 'wb')
+    tree.write(corrected_gzip)
+    corrected_gzip.close()
+    lap_timer_file.close()
+else: 
+    parser = ET.XMLParser(encoding="windows-1252")
+    tree = ET.parse(lap_timer_file, parser=parser)
+    root = tree.getroot()
 
-for speed in root.findall('.//obd/speed'):
-    corrected_speed = float(speed.text) * correction_factor
-    speed.text = str(corrected_speed)
-tree.write('corrected_speed.xml')
+    for speed in root.findall('.//obd/speed'):
+        corrected_speed = float(speed.text) * correction_factor
+        speed.text = str(corrected_speed)
+    tree.write('corrected_speed.hlptrz')
+
