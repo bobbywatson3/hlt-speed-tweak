eccf362fe966e5b57aec931947beb0b67db090df
Bobby Watson <robertwatson3@gmail.com>
2015-03-28 09:18:59 -0400

    Added input arguments

--- hlt-speed-tweak.py
+++ hlt-speed-tweak.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python
 import xml.etree.ElementTree as ET
 import gzip
+import argparse
+import os.path
 
-lap_timer_file = "LapTimer-0131-20150326-093740.hlptrz"
-correction_factor = 0.96
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', 
+            help="Input LapTimer file location.", required = True)
+parser.add_argument('-o', '--output', 
+            help='Destination file name for corrected LapTimer file. Default is "corrected_obd_speed.hlptrz"')
+parser.add_argument('-c', '--correction-factor', 
+            help="Correction factor to be applied in format '0.95' or '1.1'", required = True)
+args = parser.parse_args()
 
-if lap_timer_file.endswith('.hlptrz'):
-    print "Unzipping file"
-    lap_timer_file = gzip.open(lap_timer_file, 'rb')
+
+if args.input.endswith('.hlptrz'):
+    lap_timer_file = gzip.open(args.input, 'rb')
     lap_timer_string = lap_timer_file.read()
     root = ET.fromstring(lap_timer_string)
     for speed in root.findall('.//obd/speed'):
-        corrected_speed = float(speed.text) * correction_factor
+        corrected_speed = float(speed.text) * float(args.correction_factor)
         speed.text = str(corrected_speed)
     tree = ET.ElementTree(root)
-    corrected_gzip = gzip.open('corrected_obd_speed.hlptrz', 'wb')
+    if not args.output:
+        args.output = 'corrected_obd_speed.hlptrz'
+    corrected_gzip = gzip.open(args.output, 'wb')
     tree.write(corrected_gzip)
     corrected_gzip.close()
     lap_timer_file.close()
+    print "Corrected OBD speed LapTimer file saved to:", os.path.realpath(corrected_gzip.name)
 else: 
     parser = ET.XMLParser(encoding="windows-1252")
-    tree = ET.parse(lap_timer_file, parser=parser)
+    tree = ET.parse(args.input, parser=parser)
     root = tree.getroot()
-
     for speed in root.findall('.//obd/speed'):
-        corrected_speed = float(speed.text) * correction_factor
+        corrected_speed = float(speed.text) * float(args.correction_factor)
         speed.text = str(corrected_speed)
-    tree.write('corrected_speed.hlptrz')
-
+    if not args.output:
+        args.output = 'corrected_speed.hlptrl'
+    tree.write(args.output)
+    print "Corrected OBD speed LapTimer file saved to:", os.path.realpath(args.output)
